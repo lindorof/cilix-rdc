9efc9506d209eab1a95ecc36e25412f9a7e60d00
lindorof <gmail5@163.com>
2022-06-23 20:33:41 +0800

    .h with extern c of libs

--- libs/Linux/aarch64/cilix_rdc_ivk.h
+++ libs/Linux/aarch64/cilix_rdc_ivk.h
@@ -1,6 +1,10 @@
 #ifndef _CILIX_RDC_IVK_H_
 #define _CILIX_RDC_IVK_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "cilix_sp_ddk.h"
 #include "cilix_sp_cps.h"
 #include "cilix_sp_exp.h"
@@ -11,5 +15,9 @@ RDC_IVK_API void* rdc_init(char* phy, char* drv);
 RDC_IVK_API void rdc_fini(void* rdc);
 RDC_IVK_API int rdc_invoke(void* rdc, char* fun, char* in, int inlen, void*** out);
 RDC_IVK_API void rdc_invoke_free(void* rdc, void** out);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // #ifndef _CILIX_RDC_IVK_H_
